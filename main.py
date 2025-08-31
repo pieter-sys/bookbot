deba05d94e1095bee0411b882be3d8048aa579b9
pieter-sys <pieter.melkenbeke@gmail.com>
2025-08-31 20:55:16 +0200

    added function to count individual characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counting
+from stats import word_counting, char_counting
 
 # takes a filepath as input and returns the contents of the file as a string
 def get_book_text(path):
@@ -10,6 +10,8 @@ def get_book_text(path):
 def main():
     textbulk = get_book_text("./books/frankenstein.txt")
     num_words = word_counting(textbulk)
+    num_chars = char_counting(textbulk)
     print(f"{num_words} words found in the document")
+    print(num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,16 @@
 # takes the string of the file's contents and splits it in individual words
+
 def word_counting(bulk):
     individual_words = bulk.split()
     count = len(individual_words)
     return count
+
+def char_counting(lump):
+    characters = {}
+    lower_lump = lump.lower()
+    for i in lower_lump:
+        if i in characters:
+            characters[i] += 1
+        else:
+            characters[i] = 1
+    return characters
