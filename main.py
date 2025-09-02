0fbcc93b654d77feed9a335a5502742866818698
pieter-sys <pieter.melkenbeke@gmail.com>
2025-09-03 00:10:18 +0200

    added listify function and updated main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counting, char_counting
+from stats import word_counting, char_counting, listify
 
 # takes a filepath as input and returns the contents of the file as a string
 def get_book_text(path):
@@ -11,7 +11,15 @@ def main():
     textbulk = get_book_text("./books/frankenstein.txt")
     num_words = word_counting(textbulk)
     num_chars = char_counting(textbulk)
-    print(f"{num_words} words found in the document")
-    print(num_chars)
+    list_chars = listify(num_chars)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in list_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -5,6 +5,9 @@ def word_counting(bulk):
     count = len(individual_words)
     return count
 
+# takes the string of the file's contents, makes it all lowercase and then loops over it
+# it counts individual characters including weird ones and spaces
+
 def char_counting(lump):
     characters = {}
     lower_lump = lump.lower()
@@ -14,3 +17,17 @@ def char_counting(lump):
         else:
             characters[i] = 1
     return characters
+
+# tells sort() what to sort on (num), this function is only used inside listify
+# no need to import it
+
+def sort_on(items):
+    return items["num"]
+
+# takes the dictionary of {characters:counts} and
+# adds all the alphabetical key-value pairs to a list
+
+def listify(my_dict):
+    letters_list = [{"char": k, "num": v} for k, v in my_dict.items() if k.isalpha()]
+    letters_list.sort(reverse=True, key=sort_on)
+    return letters_list
