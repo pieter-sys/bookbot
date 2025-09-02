309aeb1217abb00bf2c5f8d3a2ccec0cddf5c457
pieter-sys <pieter.melkenbeke@gmail.com>
2025-09-03 00:32:08 +0200

    finalized the bookbot and updated the README file

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 BookBot is my first [Boot.dev](https://www.boot.dev) project!# bookbot
 Making a bookbot for boot.dev
 
-For now this does the following:
-Takes a book (.txt), it turns it into one large string, the string gets split up in individual words, the words are counted and the resulting count is printed to the terminal.
+## This does the following:
+
+1. It runs from the root folder of the project.
+2. If not exactly 2 arguments are given it exits with exit code 1 and tells you how to run it.
+3. It takes a book from a given path, it turns it into one large string, the string gets split up in individual words, the words are counted and the resulting count is printed to the terminal.
+4. It counts individual letters and displays them with their respective counts. Non-alphabetic characters are ignored.
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from stats import word_counting, char_counting, listify
+import sys
+
+# CLI argument check, suggesting proper usage if wrong
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
 
 # takes a filepath as input and returns the contents of the file as a string
+
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
 
 # main links the values so first the book turns into a string, then it gets split up and counted
+
 def main():
-    textbulk = get_book_text("./books/frankenstein.txt")
+    textbulk = get_book_text(book_path)
     num_words = word_counting(textbulk)
     num_chars = char_counting(textbulk)
     list_chars = listify(num_chars)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
